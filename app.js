c18cb3d8e1bd0c08bc0c15cbeed93b3ae4f6a46f
igkishore <gowthamkishoreindukuri@gmail.com>
2021-03-31 09:06:28 +0530

    V.06

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ var storage = multer.diskStorage({
 		cb(null, 'uploads')
 	},
 	filename: (req, file, cb) => {
-    pdf_file_name = uuidv4() + '.pdf';
+    pdf_file_name = uuidv4() + '.pdf' ;
 		cb(null, pdf_file_name)
 
 	}
